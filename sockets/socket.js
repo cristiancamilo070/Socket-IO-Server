a743cca9bce191edac48eba605f76e9aeabb4f6c
Cristian Cruz <cristiancamilo070@gmail.com>
2022-07-05 15:32:56 -0500

    Server Commit

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
 const{io}=require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands =new Bands();
+
+bands.addBand(new Band('Papoman'));
+bands.addBand(new Band('Bad bunny'));
+bands.addBand(new Band('Coffy el cafetero'));
+bands.addBand(new Band('Maluma'));
+
+console.log(bands);
 
 //mensaje de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands',bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
